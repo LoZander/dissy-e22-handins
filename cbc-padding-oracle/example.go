4046af0f95bd62e2123594f3e739707d2591bd35
gronl <gronlund@gmail.com>
2022-11-15 13:18:29 +0100

    initial start

--- cbc-padding-oracle/example.go
+++ cbc-padding-oracle/example.go
@@ -1,22 +1,35 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
 
+func printBytes(bytes []byte) {
+	for _, b := range bytes {
+		fmt.Printf("%d ", b)
+	}
+	fmt.Println()
+}
+
 func testSystemsSecurity(baseURL string) {
 	res, _ := http.Get(baseURL + "/")
 	ciphertextHex := res.Cookies()[0].Value
 	fmt.Printf("[+] received ciphertext: %s\n", ciphertextHex)
 
-	// To convert hex to bytes:
-	// ciphertextBytes, _ := hex.DecodeString(ciphertextHex)
+	ciphertextBytes, _ := hex.DecodeString(ciphertextHex)
+	ciphertextBytes[len(ciphertextBytes)-1] = 0x01
+	printBytes(ciphertextBytes)
+
+	/*send a request with a modified ciphertext*/
 
 	req, _ := http.NewRequest(http.MethodGet, baseURL+"/check/", nil)
-	req.AddCookie(&http.Cookie{Name: "authtoken", Value: ciphertextHex})
+	/*If we modify the ciphertext we get a response like: invaliud type if.  normal is plain ciphertextHex */
+
+	req.AddCookie(&http.Cookie{Name: "authtoken", Value: (hex.EncodeToString(ciphertextBytes))})
 	res, _ = http.DefaultClient.Do(req)
 	resBody, _ := ioutil.ReadAll(res.Body)
 	fmt.Printf("[+] done:\n%s\n", resBody)
